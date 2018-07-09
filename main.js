0e159ce63bc0874f5b4e7d614765651faf886c50
Nicolaj Filrup Rasmussen <nra2@balk.dk>
2018-07-09 11:27:33 +0200

    Added autocomplete support and autocomplete on bilreg.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var api_key
 
 //Status variables
 bilregOK = false;
+bilregIsSearch = false;
 
 //Function for making web requests asynchronously
 function HttpGetAsync(query, callback) {
@@ -19,6 +20,10 @@ function HttpGetAsync(query, callback) {
 function validateBilreg() {
     var field = document.getElementById('bilReg');
     
+    //Is the field empty?
+    if (field.value == "")
+        return;
+
     //Find out if we are updating an old car.
     //If we are we need to validate the plate against existing records.
     if (document.getElementById('nyBil').checked == false)
@@ -61,13 +66,40 @@ function validateBilreg() {
 
 }
 
+//Changes bilreg between input and search
+function bilregFieldState() {
+    var ny = document.getElementById("nyBil").checked;
+    
+    var bilreg = document.getElementById("bilReg")
+    var arr = [];
+
+    if (!ny) {
+        var i = 0;
+        HttpGetAsync("select distinct bilreg from lora_flaadestyring.bil_bilreg_euid", function(json) {
+            json['features'].forEach(element => {
+                arr[i] = element.properties.bilreg;
+                i++;
+            })
+        });
+
+        autocomplete(bilreg, arr);
+        bilregIsSearch = true;
+    }
+    else {
+        if (bilregIsSearch) {
+            bilregIsSearch = false;
+            removeautocomplete(bilreg);
+        }
+    }
+}
+
 // List parking places in dropdown
 function populateParkingDropdown() {
     var query = 
-        `SELECT distinct pnavn 
-        FROM lora_flaadestyring.bil_parkering_hjemme 
-        ORDER BY pnavn`
-
+    `SELECT distinct pnavn 
+    FROM lora_flaadestyring.bil_parkering_hjemme 
+    ORDER BY pnavn`
+    
     HttpGetAsync(query, function(json) {
         json['features'].forEach(element => {
             var node = document.createElement("option");
@@ -87,7 +119,7 @@ function showCarData(bilreg) {
         ON a.bilreg = b.bilreg
         WHERE a.bilreg = '` + bilreg + "'"
 
-    HttpGetAsync(query, function(json) {
+        HttpGetAsync(query, function(json) {
         json['features'].forEach(element => {
             $('#gpsID').val(element.properties.eui);
             $('#parkering').val(element.properties.pnavn);
@@ -99,7 +131,7 @@ function showCarData(bilreg) {
 function login() {
     user = String($( "#user" ).val())
     password = String($( "#pw" ).val())
-
+    
     url = "https://ballerup.mapcentia.com/api/v1/session/start"
     $.post( url, { u: user, p: password }, function( data ) {
         //Storing API key in globale variable
@@ -114,8 +146,101 @@ $( document ).ready(function() {
     $( "#gc2login" ).click(function() {
         login();
     });
-
+    
     populateParkingDropdown();
 });
 
+//Autocomplete functionality
+//Remove autocomplete to control
+function removeautocomplete(inp) {
+    let clone = inp.cloneNode();
+    while (inp.firstChild) {
+        clone.appendChild(inp.lastChild);
+    }
+    inp.parentNode.replaceChild(clone, inp);
+    clone.focus();
+}
+
+//Add autocomplete to control
+function autocomplete(inp, arr) {
+    var currentFocus;
+
+    function input_listener(e) {
+        var a, b, i, val = this.value;
+
+        closeAllLists();
+        if (!val) {return false;}
+        currentFocus = -1;
+
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+
+        this.parentNode.appendChild(a);
+
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+
+                b.innerHTML += "<input type='hidden' value = '" + arr[i] + "'>";
+
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    }
+
+    inp.addEventListener("input", input_listener);
+
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+
+        if (e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        } else if (e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    
+    function addActive(x) {
+        if (!x) return false;
+        
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    document.addEventListener("click", function(e) {
+        closeAllLists(e.target);
+    });
+}
 
